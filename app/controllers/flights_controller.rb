46645060f3fb2b5d900534a663812e7db98b0c71
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2022-06-24 11:49:36 +0700

    Successfully made the index method in flight controllers work

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,15 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    @flights = Flight.where(departure_id: flight_params[:departure_id], arrival_id: flight_params[:arrival_id], date: flight_params[:date])
+  end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:departure_id, :arrival_id, :date)
+  end
+
+  def passengers
+    params.require(:flight).permit(:passengers)
   end
 end
